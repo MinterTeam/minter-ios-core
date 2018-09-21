b98695f16b0eaa504be297cf8bcb98287018955e
Alexey Sidorov <ody344@gmail.com>
2018-09-21 17:55:05 +0300

    added digit fraction number

--- MinterCore/Classes/Helper/BigInt+Decimal.swift
+++ MinterCore/Classes/Helper/BigInt+Decimal.swift
@@ -13,6 +13,7 @@ public extension BigUInt {
 	public init?(decimal: Decimal) {
 		let formatter = NumberFormatter()
 		formatter.generatesDecimalNumbers = true
+		formatter.maximumFractionDigits = 18
 		guard let string = formatter.string(from: decimal as NSNumber) else {
 			return nil
 		}
