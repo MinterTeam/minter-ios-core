753383e045b9858d26a65fcbf1cd209f44466db6
Alexey Sidorov <ody344@gmail.com>
2020-09-25 10:34:43 +0300

    test fix

--- Example/Tests/TransactionDataTests.swift
+++ Example/Tests/TransactionDataTests.swift
@@ -35,42 +35,44 @@ class SendCoinTransactionDataSpec: BaseQuickSpec {
 		describe("SendCoinTransactionData Model") {
 			let json: [String : Any] = [
 				"to" : "Mxfe60014a6e9ac91618f5d1cab3fd58cded61ee99",
-				"coin" : "MNT",
+        "coin" : ["id": 1, "symbol": "MNT"],
 				"from" : "Mxc17a3afd805eab5ef3f2bbbc5edd49c28efb4d8c",
-				"amount" : "123",
+				"amount" : "123"
 			]
-			
-			
+
 			let model = Mapper<SendCoinTransactionDataMappable>().map(JSON: json)
 			expect(model).toNot(beNil())
 			expect(model?.to).to(equal("Mxfe60014a6e9ac91618f5d1cab3fd58cded61ee99"))
-//			expect(model?.coin).to(equal("MNT"))
+      expect(model?.coin?.id).to(equal(1))
+      expect(model?.coin?.symbol).to(equal("MNT"))
 			expect(model?.from).to(equal("Mxc17a3afd805eab5ef3f2bbbc5edd49c28efb4d8c"))
 			expect(model?.amount).to(equal(Decimal(123)))
 		}
-		
+
 		describe("ConvertTransactionDataMappable Model") {
 			it("ConvertTransactionDataMappable can be mapped") {
 				let json: [String : Any] = [
-					"coin_to_sell" : "MNT",
-					"coin_to_buy": "BELTCOIN",
-					"value" : Decimal(1.0),
+          "coin_to_sell" : ["id": 1, "symbol": "MNT"],
+					"coin_to_buy": ["id": 2, "symbol": "BELTCOIN"],
+					"value" : "1.0",
 					"from" : "Mx228e5a68b847d169da439ec15f727f08233a7ca6"]
-				
+
 				let data = ConvertTransactionDataMappable(JSON: json)
-//				expect(data?.fromCoin).to(equal("MNT"))
-//				expect(data?.toCoin).to(equal("BELTCOIN"))
+        expect(data?.fromCoin?.id).to(equal(1))
+        expect(data?.fromCoin?.symbol).to(equal("MNT"))
+        expect(data?.toCoin?.id).to(equal(2))
+        expect(data?.toCoin?.symbol).to(equal("BELTCOIN"))
 				expect(data?.value).to(equal(Decimal(1.0)))
 				expect(data?.from).to(equal("Mx228e5a68b847d169da439ec15f727f08233a7ca6"))
 			}
-			
+
 			it("ConvertTransactionDataMappable can be mapped") {
 				let json: [String : Any] = [
 					"coin_to_sell" : "MNT",
 					"coin_to_buy": "BELTCOIN",
 					"value" : "1.0",
 					"from" : "Mx228e5a68b847d169da439ec15f727f08233a7ca6"]
-				
+
 				let data = ConvertTransactionDataMappable(JSON: json)
 //				expect(data?.fromCoin).to(equal("MNT"))
 //				expect(data?.toCoin).to(equal("BELTCOIN"))
